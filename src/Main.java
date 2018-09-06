426e796bae061cd11b89543311d82ab55e5d073b
Daria.Nekrasova <913953@gmail.com>
2018-09-06 13:13:01 +0700

    com

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
         System.out.println("One more row!");
         System.out.println("One more row 2!");
 
-        System.out.println("One more row 3!");
+        System.out.println("again in master");
     }
 }
